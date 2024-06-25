1dcc8af7ad6a1eee2204d503eb19e2d00e9ce92a
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-06-25 12:44:04 +0530

    Extended OAuth2 support: Added Outlook authentication details and scopes

--- config/oauth.js
+++ config/oauth.js
@@ -8,5 +8,12 @@ const oauth2Client = new google.auth.OAuth2(
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'];
 
+const outlookOptions = {
+    clientId: process.env.OUTLOOK_CLIENT_ID,
+    clientSecret: process.env.OUTLOOK_CLIENT_SECRET,
+    redirectUri: process.env.OUTLOOK_REDIRECT_URI,
+    scope: ['https://outlook.office.com/mail.read', 'https://outlook.office.com/mail.send']
+}
 
-export { oauth2Client, SCOPES };
+
+export { oauth2Client, SCOPES, outlookOptions };
