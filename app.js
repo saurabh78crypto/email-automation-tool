c83322596325bfebfc219261efa6bcf2f494172f
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-06-25 13:13:01 +0530

    Implemented Google OAuth2 callback and authorization endpoints

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import session from 'express-session';
 import dotenv from 'dotenv';
 import moment from 'moment';
+import {oauth2Client, SCOPES} from './config/oauth.js'
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,17 @@ const {PORT} = process.env;
 app.use(express.json());
 app.use(session({ secret: 'secret-key', resave: false, saveUninitialized: true }));
 
+app.get('/google', (req, res) => {
+    const authUrl = oauth2Client.generateAuthUrl({ access_type: 'offline', scope: SCOPES});
+    res.redirect(authUrl);
+});
+
+app.get('/google/callback', async (req, res) => {
+    const {tokens} = await oauth2Client.getToken(req.query.code);
+    oauth2Client.setCredentials(tokens);
+    req.session.googleTokens = tokens;
+    res.send('Google authentication successful!')
+});
 
 
 app.listen(PORT, () => {
