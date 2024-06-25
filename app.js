e938ddd67817c4f6807e1cef56187925392254c3
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-06-25 13:24:58 +0530

    Implemented Outlook OAuth2 callback and authorization endpoints

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from 'express';
 import session from 'express-session';
 import dotenv from 'dotenv';
 import moment from 'moment';
-import {oauth2Client, SCOPES} from './config/oauth.js'
+import axios from 'axios';
+import {oauth2Client, SCOPES, outlookOptions} from './config/oauth.js'
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,30 @@ app.get('/google/callback', async (req, res) => {
     oauth2Client.setCredentials(tokens);
     req.session.googleTokens = tokens;
     res.send('Google authentication successful!')
+});
+
+
+app.get('/outlook', (req, res) => {
+    const authUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize? 
+ client_id=${outlookOptions.clientId}&response_type=code&redirect_uri=${outlookOptions.redirectUri}&respons 
+ e_mode=query&scope=${outlookOptions.scope.join(' ')}&state=12345`;
+
+    res.redirect(authUrl);
+});
+
+app.get('/outlook/callback', async (req, res) => {
+    const {code} = req.query;
+    const tokenResponse = await axios.post(`https://login.microsoftonline.com/common/oauth2/v2.0/token`, null, {
+        params: {
+            client_id: outlookOptions.clientId,
+            client_secret: outlookOptions.clientSecret,
+            redirect_uri: outlookOptions.redirectUri,
+            code,
+            grant_type: 'authorization_code'
+        }
+    });
+    req.session.outlookTokens = tokenResponse.data;
+    res.send('Outlook authentication successful!')
 });
 
 
